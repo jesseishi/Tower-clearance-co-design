a7f8028d59aaad2b5b3f99bbe66134b43c60634d
Jesse Hummel <j.i.s.hummel@tudelft.nl>
2025-11-16 20:21:16 +0100

    First few optimizations with AEP vs tower clearance.

--- scripts/optimal_tuning/weis_driver.sh
+++ scripts/optimal_tuning/weis_driver.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name="weis" 
 #SBATCH --partition=compute-p1
-#SBATCH --time=03:59:00 
+#SBATCH --time=10:00:00 
 #SBATCH --ntasks=32
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2GB
