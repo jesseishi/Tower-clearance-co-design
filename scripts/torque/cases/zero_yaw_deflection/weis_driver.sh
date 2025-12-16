5b8d18a779ff7477caf7aff284d59c711af5cfbc
Jesse Hummel <j.i.s.hummel@tudelft.nl>
2025-12-16 10:39:22 +0100

    Prepare parameter sweeps for DelftBlue.

---
+++ scripts/torque/cases/zero_yaw_deflection/weis_driver.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+#SBATCH --job-name="weis" 
+#SBATCH --partition=compute-p1
+#SBATCH --time=03:59:00 
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --account=innovation
+#SBATCH --mail-type=ALL
+
+# Load necessary modules. The intel module is needed to run OpenFAST
+# (libmkl_gf_lp64.so.2).
+module load 2025
+module load intel/oneapi-all
+module load miniconda3
+
+# If python buffers print statements it becomes a lot harder to debug. So let's
+# not allow buffering for now.
+export PYTHONUNBUFFERED=1
+
+# See: https://doc.dhpc.tudelft.nl/delftblue/Slurm-scheduler/#intel-mpi-job
+export I_MPI_PMI_LIBRARY=/cm/shared/apps/slurm/current/lib64/libpmi2.so
+
+# And run in the conda environment.
+conda activate tip_clearance
+echo "Python executable: $(which python)."
+echo "Running weis_driver.py now..."
+srun python weis_driver.py
+conda deactivate
